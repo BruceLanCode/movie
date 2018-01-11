66e1d714ba6b0245afc9f70ef7b6ae079392952b
lantuhui <shengxiaobai15@163.com>
2018-01-11 21:03:13 +0800

    add index detail admin pages
    add index detail admin pages

--- app.js
+++ app.js
@@ -1,27 +1,74 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const Pug = require('koa-pug');
+const staticServer = require('koa-static');
 const path = require('path');
 const port = process.env.PORT || 3000;
 
 const app = new Koa();
 const pug = new Pug({
-    viewPath: 'views',
+    viewPath: 'views/pages',
     app
 });
 
 app.listen(port);
 
 const router = new Router();
-router.get('/huang',async (ctx, next) => {
-    ctx.render('layout',{title: 'huang'});
+router.get('/',async (ctx, next) => {
+    ctx.render('index',{
+        title: 'lantu',
+        movies: [{
+            title: '机械战警',
+            _id: 1,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+        },{
+            title: '机械战警',
+            _id: 1,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+        },{
+            title: '机械战警',
+            _id: 1,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+        },{
+            title: '机械战警',
+            _id: 1,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+        },{
+            title: '机械战警',
+            _id: 1,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+        },{
+            title: '机械战警',
+            _id: 1,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+        }]
+    });
     next();
 });
-router.get('/xi',async (ctx, next) => {
-    ctx.render('layout',{title: 'xi'});
+router.get('/movie/:id',async (ctx, next) => {
+    ctx.render('detail',{
+        title: '《机械战警》',
+        movie: {
+            director: '何塞·帕迪里亚',
+            country: '美国',
+            title: '机械战警',
+            year: 2014,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+            language: '英语',
+            flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
+            summary: '翻拍自1987年同名科幻经典、由《精英部队》导演何塞·帕迪里亚执导的新版《机械战警》发布首款预告片。大热美剧《谋杀》男星乔尔·金纳曼化身新“机械战警”酷黑战甲亮相，加里·奥德曼、塞缪尔·杰克逊、迈克尔·基顿三大戏骨绿叶护航。预告片末更亮出了本片将登陆IMAX巨幕。新版《机械战警》故事背景跟原版一样，依旧设定在工业城市底特律，但故事年代已由之前设定的2020年变为了2028年，并且故事格局也明显扩大。在片中，金纳曼饰演的好警察墨菲将会被歹徒“杀死”，然后被进行军火开发的机器人公司Omni Corp改造成半人半机器的“机械战警”。'
+        }
+    });
+    next();
+});
+router.get('/admin',async (ctx, next) => {
+    ctx.render('admin',{
+        title: '后台录入页'
+    });
     next();
 });
 
 app.use(router.routes());
+app.use(staticServer(path.join(__dirname,'public')));
 
 console.info('Server started port 3000');
