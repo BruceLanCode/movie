70f2f5d96df675c0938d5e9a1eadc1d3376b168e
lantuhui <shengxiaobai15@163.com>
2018-01-16 08:49:48 +0800

    change framework
    change framework

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const Router = require('koa-router');
 const koaBody = require('koa-body');
 const Pug = require('koa-pug');
 const staticServer = require('koa-static');
+const moment = require('moment');
 const path = require('path');
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
@@ -35,52 +36,80 @@ router.get('/',async (ctx, next) => {
 });
 //detail page
 router.get('/movie/:id',async (ctx, next) => {
-    await Movie.findById(ctx.params.id, function (err, movie) {
+    let id = ctx.params.id;
+
+    await Movie.findById(id, function (err, movie) {
         if(err !== null) {
             console.log(err);
         }
         ctx.render('detail',{
-            title: `movie 《${movie.title}》`,
+            title: `movie 详情页`,
             movie: movie
         });
     });
     await next();
 });
-//admin page
-router.get('/admin/new/:id?', async (ctx, next) => {
+//admin new page
+router.get('/admin/new', async (ctx, next) => {
+    ctx.render('admin', {
+        title: 'movie 后台录入页',
+        movie: {
+            title: '',
+            director: '',
+            country: '',
+            year: '',
+            poster: '',
+            flash: '',
+            summary: '',
+            language: '',
+        }
+    });
+    await next();
+})
+
+//admin update page
+router.get('/admin/update/:id', async (ctx, next) => {
     let id = ctx.params.id;
     if (id) {
         await Movie.findById(id, (err, movie) => {
-            if(err) {
-                console.log(err);
-            }
             ctx.render('admin', {
-                title: 'movie 后台修改' + movie.title,
+                title: 'movie 后台更新页',
                 movie: movie
             });
         });
     }
-    else {
-        await ctx.render('admin', {
-            title: 'movie 后台录入页',
-            movie: {}
-        })
-    }
     await next();
 })
-
+//admin post movie
 router.post('/admin/movie', koaBody(), async (ctx, next) => {
-    let _movie = ctx.request.body.movie;
-    if(!_movie._id) {
+    let movieObj = ctx.request.body.movie;
+    let id = ctx.request.body.movie._id;
+    let _movie;
+    if(id !== 'undefined') {
+        await Movie.findById(id, async (err, movie) => {
+            if(err) {
+                console.log(err);
+            }
+            _movie = Object.assign(movie, movieObj);
+            await _movie.save((err, movie) => {
+                if(err) {
+                    console.log(err);
+                }
+
+                ctx.redirect('/movie/' + movie._id);
+            })
+        })
+    }
+    else {
         _movie = new Movie({
-            director: ctx.request.body.movie.director,
-            title: ctx.request.body.movie.title,
-            language: ctx.request.body.movie.language,
-            country: ctx.request.body.movie.country,
-            year: ctx.request.body.movie.year,
-            poster: ctx.request.body.movie.poster,
-            flash: ctx.request.body.movie.flash,
-            summary: ctx.request.body.movie.summary,
+            director: movieObj.director,
+            title: movieObj.title,
+            language: movieObj.language,
+            country: movieObj.country,
+            year: movieObj.year,
+            poster: movieObj.poster,
+            flash: movieObj.flash,
+            summary: movieObj.summary,
         });
 
         await _movie.save((err, movie) => {
@@ -91,21 +120,6 @@ router.post('/admin/movie', koaBody(), async (ctx, next) => {
             ctx.redirect('/movie/' + movie._id);
         })
     }
-    else {
-        await Movie.findById(_movie._id, async (err, movie) => {
-            if(err) {
-                console.log(err);
-            }
-            _movie = Object.assign(movie, _movie);
-            await _movie.save((err, movie) => {
-                if(err) {
-                    console.log(err);
-                }
-
-                ctx.redirect('/movie/' + movie._id);
-            })
-        })
-    }
     await next();
 });
 //list page
@@ -114,12 +128,12 @@ router.get('/admin/list', async (ctx, next) => {
         if(err) {
             console.log(err)
         }
-
+        movies.forEach(movie => {
+            movie.meta.updateAt = moment(movie.meta.updateAt).format('MM/DD/YYYY');
+        });
         ctx.render('list', {
-            title: 'movie列表页',
-            documents: {
-                results: movies
-            }
+            title: 'movie 列表页',
+            results: movies
         })
     });
     await next()
@@ -129,9 +143,10 @@ router.del('/admin/list', async (ctx, next) => {
     let id = ctx.query.id;
 
     if(id) {
-        await Movie.remove({_id: id}, (err, home) => {
+        await Movie.remove({_id: id}, (err, movie) => {
             if(err) {
                 console.log(err);
+                ctx.body = {success: 0};
             }
             else {
                 ctx.body = {success: 1};
